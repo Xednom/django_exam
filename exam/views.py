b918109bea13a0f34d57275cb47c80569506868e
Richmond <monde.lacanlalay@gmail.com>
2018-03-11 19:42:37 +0800

    Updated: fibonacci sequence with list

--- exam/views.py
+++ exam/views.py
@@ -31,9 +31,11 @@ def fibo(request, n):
 
 
 def fib(n):
-    if(n == 0):
-        return 0
-    elif(n == 1):
-        return 1
+    if n == 0:
+        return [0]
+    elif n == 1:
+        return [0, 1]
     else:
-        return fib(n-1) + fib(n-2)
+        result = fib(n-1)
+        result.append(result[-1] + result[-2])
+        return result
